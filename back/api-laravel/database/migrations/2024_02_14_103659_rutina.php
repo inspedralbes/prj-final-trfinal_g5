2012d3d1bfef38665cdf5c031c78de953b803a6b
a21aledelfel <a21aledelfel@inspedralbes.cat>
2024-02-14 11:56:20 +0100

    update ejercicios y añadir tabla Rutina

--- back/api-laravel/database/migrations/2024_02_14_103659_rutina.php
+++ back/api-laravel/database/migrations/2024_02_14_103659_rutina.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('rutinas', function (Blueprint $table) {
             $table->id();
             $table->foreignId('usuari_id')->constrained('usuaris')->onDelete('cascade');
-            $table->string('nom');
+            $table->string('nombre'); // Cambiado el nombre de la columna para la relación
+            $table->foreign('nombre')->references('nombre')->on('ejercicios'); // Definiendo la relación
             $table->string('series')->nullable();
-            $table->string('repeticions')->nullable();
-
+            $table->string('repeticiones')->nullable();
         });
     }
 
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('rutinas');
     }
 };
