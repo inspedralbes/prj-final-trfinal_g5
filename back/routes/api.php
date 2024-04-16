af65f340330c9463839888899562b7a8cc88fcdd
Aitor <a20aitbaresc@inspedralbes.cat>
2024-04-16 12:06:10 +0200

    quitar creardieta

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,13 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\DietaControlador;
 
 
 Route::get('/usuari', [UserController::class, 'usuari']);
 Route::get('/usuari/{id}', [UserController::class, 'usuariId']);
 Route::post('/editar-usuari{id}', [UserController::class, 'editarUsuari']);
-Route::post('/crear-dieta', [DietaControlador::class, 'crearDieta']);
 Route::post('/registre', [UserController::class, 'registre']);
 Route::post('/loguejat', [UserController::class, 'loguejat']);
 Route::post('/deslojegat', [UserController::class, 'deslojegat']);
