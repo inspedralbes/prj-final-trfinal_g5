c1a460c24eaf4e177c7200280e99cd22fa93ecd6
Alex D <a21aledelfel@inspedralbes.cat>
2024-04-23 10:29:26 +0200

    Seeder Categories

--- back/database/seeders/CategoriasTable.php
+++ back/database/seeders/CategoriasTable.php
@@ -14,6 +14,69 @@ class CategoriasTable extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                "idCategoria" => 1,
+                "nombreCategoria" => "Bíceps",
+                "imagenCategoria" => "imagen_biceps.jpg"
+            ],
+            [
+                "idCategoria" => 2,
+                "nombreCategoria" => "Abdominales",
+                "imagenCategoria" => "imagen_abdominales.jpg"
+            ],
+            [
+                "idCategoria" => 3,
+                "nombreCategoria" => "Abductores",
+                "imagenCategoria" => "imagen_abductores.jpg"
+            ],
+            [
+                "idCategoria" => 4,
+                "nombreCategoria" => "Pectoral",
+                "imagenCategoria" => "imagen_pectoral.jpg"
+            ],
+            [
+                "idCategoria" => 5,
+                "nombreCategoria" => "Glúteos",
+                "imagenCategoria" => "imagen_gluteos.jpg"
+            ],
+            [
+                "idCategoria" => 6,
+                "nombreCategoria" => "Isquiotibiales",
+                "imagenCategoria" => "imagen_isquiotibiales.jpg"
+            ],
+            [
+                "idCategoria" => 7,
+                "nombreCategoria" => "Dorsales",
+                "imagenCategoria" => "imagen_dorsales.jpg"
+            ],
+            [
+                "idCategoria" => 8,
+                "nombreCategoria" => "Espalda Baja",
+                "imagenCategoria" => "imagen_espalda_baja.jpg"
+            ],
+            [
+                "idCategoria" => 9,
+                "nombreCategoria" => "Espalda Media",
+                "imagenCategoria" => "imagen_espalda_media.jpg"
+            ],
+            [
+                "idCategoria" => 10,
+                "nombreCategoria" => "Cuádriceps",
+                "imagenCategoria" => "imagen_cuadriceps.jpg"
+            ],
+            [
+                "idCategoria" => 11,
+                "nombreCategoria" => "Trapecios",
+                "imagenCategoria" => "imagen_trapecios.jpg"
+            ],
+            [
+                "idCategoria" => 12,
+                "nombreCategoria" => "Tríceps",
+                "imagenCategoria" => "imagen_triceps.jpg"
+            ]
+        ];
+        DB::table('categorias')->insert($data);
+
     }
 }
