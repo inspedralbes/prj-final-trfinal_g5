6602de45b35fc3f1d112fe0a4d06e92847c3463d
Aitor <a20aitbaresc@inspedralbes.cat>
2024-04-30 09:41:31 +0200

    quitar altura

--- front/pages/perfil/[nom_usuari].vue
+++ front/pages/perfil/[nom_usuari].vue
@@ -32,12 +32,6 @@
                             <input type="date" v-model="usuario.data_naixement" @change="validateFecha">
 
                         </div>
-
-                        <div class="input-container">
-                            <label>Altura:</label>
-                            <input type="text" placeholder="Altura (cm)" v-model="usuario.altura"
-                                @input="validateAltura" maxlength="4">
-                        </div>
                         <div class="input-container">
                             <label>Pes:</label> <br>
                             <input type="text" placeholder="Pes (kg)" v-model="usuario.pes" @input="validatePeso"
