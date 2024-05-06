07bcf73b3657ea57b36eb98266c46d06f5066b59
a22erirodnos <a22erirodnos@inspedralbes.cat>
2024-05-06 09:06:23 +0200

    corregir merge

--- front/pages/home.vue
+++ front/pages/home.vue
@@ -1,122 +1,121 @@
 <template>
-    <body>
-      <div class="flex-container">
-        <capçalera />
-        <div class="button-container">
-          <button class="large-button rutina-button" @click="redirectTo('rutina')">
-            Rutina
-          </button>
-          <button class="large-button dieta-button" @click="redirectTo('dieta')">
-            Dieta
+  <body>
+    <div class="flex-container">
+      <capçalera />
+      <div class="button-container">
+        <button class="large-button rutina-button" @click="redirectTo('rutina')">
+          Rutina
+        </button>
+        <button class="large-button dieta-button" @click="redirectTo('dieta')">
+          Dieta
 
-          </button>
-          <button class="large-button asesoramiento-button" @click="redirectTo('/chatia')">
-            Assesorament
+        </button>
+        <button class="large-button asesoramiento-button" @click="redirectTo('/chatia')">
+          Assesorament
 
-          </button>
-        </div>
+        </button>
       </div>
-      <navBar />
-    </body>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      redirectTo(page) {
-        this.$router.push(page);
-      }
+    </div>
+    <navBar />
+  </body>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectTo(page) {
+      this.$router.push(page);
     }
   }
-  </script>
-  
-  <style scoped>
-  /* Estilos de los divs en el componente */
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    overflow-x: hidden;
-    /* Evita el desplazamiento horizontal */
-  }
-  
-  .page-enter-active,
-  .page-leave-active {
-    transition: all 0.4s;
-  }
-  .page-enter-from,
-  .page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-  }
-  
-  .flex-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 95vh;
-    /* Mínimo 100% de la altura de la ventana */
-    background-color: #FFF;
-    position: relative;
-  }
-  
-  .button-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    /* Empuja hacia abajo los botones */
-  }
-  
-  .large-button {
-    position: relative; /* Añade posición relativa al botón */
-    width: 160%;
-    /* Ancho del 80% del contenedor padre */
-    max-width: 400px;
-    height: 100px;
-    margin-bottom: 50px;
-    /* Espacio entre los botones */
-    font-size: 24px;
-    font-weight: bold;
-    color: #fff;
-    cursor: pointer;
-    border: none;
-    outline: none;
-    background-size: cover;
-    border-radius: 10px;
-  }
-  
-  .rutina-button {
-    margin-top: 2.6em;
-    background-image: linear-gradient(to right, #ff7300, #FFA500);
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    background-position: center;
-    font-size: 2.2em;
-    color: #474747;
-  }
-  
-  .dieta-button {
-    background-image: linear-gradient(to right, #ff7300, #FFA500);
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    background-position: center;
-    font-size: 2.2em;
-    color: #474747;
-  }
-  
-  .asesoramiento-button {
-    background-image: linear-gradient(to right, #ff7300, #FFA500);
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    background-position: center;
-    font-size: 2.2em;
-    color: #474747;
-  }
-  
-  
-  
-  navBar {
-    position: fixed;
-    width: 100%;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+/* Estilos de los divs en el componente */
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow-x: hidden;
+  /* Evita el desplazamiento horizontal */
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 95vh;
+  /* Mínimo 100% de la altura de la ventana */
+  background-color: #FFF;
+  position: relative;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* Empuja hacia abajo los botones */
+}
+
+.large-button {
+  position: relative; /* Añade posición relativa al botón */
+  width: 160%;
+  /* Ancho del 80% del contenedor padre */
+  max-width: 400px;
+  height: 100px;
+  margin-bottom: 50px;
+  /* Espacio entre los botones */
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background-size: cover;
+  border-radius: 10px;
+}
+
+.rutina-button {
+  margin-top: 2.6em;
+  background-image: linear-gradient(to right, #ff7300, #FFA500);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  background-position: center;
+  font-size: 2.2em;
+  color: #474747;
+}
+
+.dieta-button {
+  background-image: linear-gradient(to right, #ff7300, #FFA500);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  background-position: center;
+  font-size: 2.2em;
+  color: #474747;
+}
+
+.asesoramiento-button {
+  background-image: linear-gradient(to right, #ff7300, #FFA500);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  background-position: center;
+  font-size: 2.2em;
+  color: #474747;
+}
+
+
+
+navBar {
+  position: fixed;
+  width: 100%;
+}
+</style>
