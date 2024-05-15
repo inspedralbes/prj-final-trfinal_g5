1a89911e980d04997270dd6c51f75ae8ea6c271f
Miquelbarcelo04 <a16miqbargim@inspedralbes.cat>
2024-05-15 13:02:32 +0200

    mostrar datos de cada usuario

--- front/pages/usuaris/[id_usuari].vue
+++ front/pages/usuaris/[id_usuari].vue
@@ -1,19 +1,33 @@
 <template>
-    <div >
-        <h1>Detalles del Usuario</h1>
-        <p><strong>Nombre:</strong> {{ usuario.nom }}</p>
-        <p><strong>Apellidos:</strong> {{ usuario.cognoms }}</p>
-        <p><strong>Email:</strong> {{ usuario.email }}</p>
-        <p><strong>Fecha de Nacimiento:</strong> {{ usuario.data_naixement }}</p>
-        <p><strong>Género:</strong> {{ usuario.genere }}</p>
-        <p><strong>Peso:</strong> {{ usuario.pes }}</p>
-        <p><strong>Altura:</strong> {{ usuario.altura }}</p>
-        <p><strong>Teléfono:</strong> {{ usuario.telefon }}</p>
-    </div>
+    <body>
+        <div class="flex-container">
+            <capçalera />
+            <h1>Detalles del Usuario</h1>
+            <div v-if="usuario">
+                <p><strong>Nom:</strong> {{ usuario.nom }}</p>
+                <p><strong>Congons:</strong> {{ usuario.cognoms }}</p>
+                <p><strong>Email:</strong> {{ usuario.email }}</p>
+                <p><strong>Data de Nacimiento:</strong> {{ usuario.data_naixement }}</p>
+                <p><strong>Génere:</strong> {{ usuario.genere }}</p>
+                <p><strong>Pes:</strong> {{ usuario.pes }}</p>
+                <p><strong>Altura:</strong> {{ usuario.altura }}</p>
+                <p><strong>Teléfon:</strong> {{ usuario.telefon }}</p>
+
+                <button @click="borrarUsuari(usuario.id)">Borrar Usuari</button>
+                <button>Rutines Usuari</button>
+                <button>Dietes Usuari</button>
+            </div>
+            <div v-else>
+                <p>Carregant dades de l'usuari...</p>
+            </div>
+            <navBar />
+        </div>
+    </body>
 </template>
 
 <script>
 import { useUsuariPerfilStore } from '@/stores/index';
+
 export default {
     data() {
         return {
@@ -28,8 +42,12 @@ export default {
             const store = useUsuariPerfilStore();
             const idUsuario = store.id_usuari;
             getDatosUsuario2(idUsuario)
-                .then(data => {
-                    this.usuario = data.usuario;
+                .then(response => {
+                    if (response.status === 1) {
+                        this.usuario = response.usuario;
+                    } else {
+                        console.error('Error al obtener los datos del usuario');
+                    }
                 })
                 .catch(error => {
                     console.error(error);
@@ -40,9 +58,29 @@ export default {
 </script>
 
 <style scoped>
+html,
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    overflow-x: hidden;
+    /* Evita el desplazamiento horizontal */
+}
+
+.flex-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    /* Mínimo 100% de la altura de la ventana */
+    background-color: #FFF;
+    position: relative;
+}
+
 h1 {
     margin-bottom: 20px;
 }
+
 p {
     margin: 5px 0;
 }
