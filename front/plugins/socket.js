4c34222974de3dcff6779529f102125f23b13b2e
Aitor <a20aitbaresc@inspedralbes.cat>
2024-05-16 12:10:30 +0200

    s

--- front/plugins/socket.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:3000'); // Cambia la URL con la dirección de tu servidor WebSocket
-
-Vue.prototype.$socket = socket;
